fe2f0ef31bc55848868c57e5f2b4b9149a8bf8c9
hunter_out <hunter_out@icloud.com>
2020-04-08 04:39:24 +0300

    добавил create, update, delete view в views, urls; добавил пагинатор; изменил ситис хом

--- cities/views.py
+++ cities/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.paginator import Paginator
+from django.urls import reverse_lazy
 from .models import City
 from .forms import CityForm
 
 def home(request):
-    if request.method == 'POST':
-        form = CityForm(request.POST or None)
-        if form.is_valid():
-            print(form.cleaned_data)
-    form = CityForm()
-    city = request.POST.get('name')
     cities = City.objects.all()
-    return render(request, 'cities/home.html', {'objects_list': cities, 'form': form})
+    paginator = Paginator(cities, 6)
+    page = request.GET.get('page')
+    cities = paginator.get_page(page)
+    return render(request, 'cities/home.html', {'objects_list': cities})
+
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     context_object_name = 'object'
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('city:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('city:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
+    success_url = reverse_lazy('city:home')
