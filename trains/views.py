d6a8b40fa7b17cb922fea39fa4aee4f1fed8841f
hunter_out <hunter_out@icloud.com>
2020-04-15 01:22:34 +0300

    доделал приложение поезда

--- trains/views.py
+++ trains/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Train
 from .forms import TrainForm
 
@@ -20,3 +22,26 @@ class TrainCreateView(SuccessMessageMixin, CreateView):
     template_name = 'trains/create.html'
     success_url = reverse_lazy('train:home')
     success_message = 'Поезд успешно создан!'
+
+
+class TrainDetailView(DetailView):
+    queryset = Train.objects.all()
+    context_object_name = 'object'
+    template_name = 'trains/detail.html'
+
+
+class TrainUpdateView(SuccessMessageMixin, UpdateView):
+    model = Train
+    form_class = TrainForm
+    template_name = 'trains/update.html'
+    success_url = reverse_lazy('train:home')
+    success_message = 'Поезд успешно отредактирован!'
+
+
+class TrainDeleteView(DeleteView):
+    model = Train
+    success_url = reverse_lazy('train:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Поезд успешно удален!')
+        return self.post(request, *args, **kwargs)
