062fe78c176d39e7c2372090ff53cb9887206f03
hunter_out <hunter_out@icloud.com>
2020-04-16 05:19:54 +0300

    исправил ошибку в формс

--- routes/forms.py
+++ routes/forms.py
@@ -1,16 +1,17 @@
 from django import forms
 from cities.models import City
 
+
 class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(label='Откуда', queryset=City.objects.all(),
-                                       widget=forms.Select(attrs={'class': 'form-control js-example-basic-single',}))
+                                       widget=forms.Select(attrs={'class': 'form-control js-example-basic-single', }))
     to_city = forms.ModelChoiceField(label='Куда', queryset=City.objects.all(),
-                                     widget=forms.Select(attrs={'class': 'form-control js-example-basic-single',}))
-    across_sities = forms.ModelMultipleChoiceField(label='Через города', queryset=City.objects.all(),
-                                                    required=False,
-                                                    widget=forms.SelectMultiple(
-                                                        attrs={'class': 'form-control js-example-basic-multiple',}))
-    traveling_time = forms.IntegerField(label='Время в пути',
-                                     widget=forms.NumberInput(
-                                         attrs={'class': 'form-control',
-                                                'placeholder': 'Введите время в пути'}))
+                                     widget=forms.Select(attrs={'class': 'form-control js-example-basic-single', }))
+    across_cities = forms.ModelMultipleChoiceField(label='Через города', queryset=City.objects.all(),
+                                                   required=False,
+                                                   widget=forms.SelectMultiple(
+                                                       attrs={'class': 'form-control js-example-basic-multiple', }))
+    travelling_time = forms.IntegerField(label='Время в пути',
+                                         widget=forms.NumberInput(
+                                             attrs={'class': 'form-control',
+                                                    'placeholder': 'Введите время в пути'}))
