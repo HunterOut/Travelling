40c34fb99df80afca3ad21a11e80be4044be9dfd
hunter_out <hunter_out@icloud.com>
2020-04-22 07:15:32 +0300

    удалил не нужную views

--- travel/views.py
+++ travel/views.py
@@ -1,6 +1,2 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-def home_view(request):
-    context = {"name": "Kolya"}
-    return render(request, "home.html", context)
